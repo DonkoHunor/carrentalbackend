2a6ef65f8fe1b54e5e077a2e21c1bc797b3d4fe9
Hunor Donkó <donko.hunor.attila@gmail.com>
2024-04-24 10:12:55 +0200

    endpoints

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
+import { UrlWithStringQuery } from 'url';
 
-@Controller('cars')
+@Controller('api/cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
   @Post()
   create(@Body() createCarDto: CreateCarDto) {
     return this.carsService.create(createCarDto);
+  }
+
+  @Post('/:car/rent')
+  rent(@Param('car') carString: string) {
+    return this.carsService.rent(carString);
   }
 
   @Get()
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,15 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class CarsService {
+  constructor(private readonly prisma: PrismaService){}
+
   create(createCarDto: CreateCarDto) {
-    return 'This action adds a new car';
+    return this.prisma.cars.create({
+      data: {
+        license_plate_number: createCarDto.plate_number,
+        brand: createCarDto.brand,
+        model: createCarDto.model,
+        daily_cost: createCarDto.daily_cost
+      }
+    });
+  }
+
+  async rent(carString: string){
+    const carId = parseInt(carString);
+    const date_start = new Date(Date.now());
+    const date_end = new Date(date_start);
+    date_end.setDate(date_end.getDate() + 7);
+
+    const car = await this.prisma.cars.findFirst({where: {id: carId}});
+    if(car === undefined) {
+      throw new NotFoundException('There is no such car in the database');
+    }
+
+    const rented = await this.prisma.rentals.findFirst({
+      where: {
+        car_id: car.id,
+        end_date: {gt: date_start}
+      }
+    });
+
+    if(rented !== undefined) {
+      throw new ConflictException('The car is already rented');
+    }
+
+    return this.prisma.rentals.create({
+      data: {
+        car_id: car.id,
+        start_date: date_start,
+        end_date: date_end
+      }
+    });
   }
 
   findAll() {
-    return `This action returns all cars`;
+    return this.prisma.cars.findMany();
   }
 
   findOne(id: number) {
